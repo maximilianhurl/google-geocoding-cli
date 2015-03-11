ffb563ccbccf532f51ff0764e03a716274738358
Max Hurl <max@dabapps.com>
2015-03-11 00:58:12 +0000

    Added tests for manager

--- scripts/manager.py
+++ scripts/manager.py
@@ -30,6 +30,7 @@ class GeocodingManager():
 
     def search(self):
         # loop over data and conduct search query
+
         self.file_handler.setup_files()
 
         for row in self.file_handler.get_rows():
@@ -45,14 +46,15 @@ class GeocodingManager():
 
     def search_row(self, row):
         time.sleep(0.1)  # make sure we dont do too many requests
-        try:
-            row = self.search_handler.search(row)
-        except GeocoderException:
-            pass
-        except GeocoderOverLimitException:
-            click.echo("Over query limit exception")
-            self.switch_key()
-            self.search_row(row)  # retry with new key
+        if not self.overAPILimit:
+            try:
+                row = self.search_handler.search(row)
+            except GeocoderException:
+                pass
+            except GeocoderOverLimitException:
+                click.echo("Over query limit exception")
+                self.switch_key()
+                self.search_row(row)  # retry with new key
 
         return row
 
@@ -65,8 +67,9 @@ class GeocodingManager():
 
     def switch_key(self):
         # switch to next key if there are anymore
-        if (len(self.keys) > self.current_key_index):
+        if (len(self.keys) - 1 > self.current_key_index):
             self.current_key_index += 1
-            self.search_handler.set_key(self.keys[self.current_key_index])
+            if self.search_handler:
+                self.search_handler.set_key(self.keys[self.current_key_index])
         else:
             self.overAPILimit = True
